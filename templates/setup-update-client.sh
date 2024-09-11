aa401c3c847f9946340bf5e08cb1d6315e1ec079
scottf <scottfauerbach@gmail.com>
2024-09-11 08:32:04 -0400

    WIP 45 Fix update script

---
+++ templates/setup-update-client.sh
@@ -0,0 +1,15 @@
+cd ~
+mkdir -p bin
+rm bin/update-client
+cat > bin/update-client <<EOF
+
+cd ~
+rm -rf testing.java
+git clone https://github.com/synadia-io/testing.java
+cd testing.java
+
+chmod +x bin/* && chmod -x bin/*.bat && bin/make && bin/get-aws && bin/generate && chmod +x gen/*
+EOF
+
+chmod +x bin/*
+echo "Todo: Export AWS security"
