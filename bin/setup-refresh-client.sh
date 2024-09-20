019e3cdc47bd83843ff3dd427490c5e3f582fc36
scottf <scottfauerbach@gmail.com>
2024-09-20 10:29:38 -0400

    WIP 53 Local, Which

--- bin/setup-refresh-client.sh
+++ bin/setup-refresh-client.sh
@@ -8,6 +8,13 @@ rm -rf testing.java
 git clone https://github.com/synadia-io/testing.java
 cd testing.java
 
+cat > generator.json <<REOF
+{
+  "server_filter": "scottf-server-",
+  "client_filter": "scottf-client-"
+}
+REOF
+
 chmod +x bin/* && chmod -x bin/*.bat && bin/make && bin/get-aws && bin/gen && chmod +x gen/*
 EOF
 
