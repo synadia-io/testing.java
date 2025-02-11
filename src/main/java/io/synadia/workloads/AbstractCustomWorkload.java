2d34b55e86ad09821228169acda889add5debf0e
scottf <scottfauerbach@gmail.com>
2025-02-11 12:24:37 -0500

    WIP 174

--- src/main/java/io/synadia/workloads/AbstractCustomWorkload.java
+++ src/main/java/io/synadia/workloads/AbstractCustomWorkload.java
@@ -40,8 +40,6 @@ public abstract class AbstractCustomWorkload extends Workload {
     protected String watchDateFormat;
 
     public void customWorkloadInit(String defaultLabel, boolean requiresArguments, CommandLine commandLine) {
-        Debug.PRINT_TIME = false;
-
         init(defaultLabel, commandLine);
         if (requiresArguments && commandLine.args.isEmpty()) {
             exit("Argument(s) Required");
@@ -287,7 +285,7 @@ public abstract class AbstractCustomWorkload extends Workload {
         WorkState ws = new WorkState();
         for (int tix = 0; tix < threadCount; tix++) {
             Thread t = new Thread(worker.getWork(options.get(tix), tix, ws));
-            t.setName("" + job.charAt(0) + tix);
+            t.setName("Thread " + tix + " ");
             t.start();
             threads.add(t);
         }
