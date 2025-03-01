ee69816f0731e8d8b493f9a436562d358825d64d
scottf <scottfauerbach@gmail.com>
2025-03-01 12:20:11 -0500

    WIP 223

--- src/main/java/io/synadia/workloads/ConsumerInfoSim.java
+++ src/main/java/io/synadia/workloads/ConsumerInfoSim.java
@@ -222,7 +222,7 @@ public class ConsumerInfoSim extends AbstractCustomWorkload {
                 long siCount = si.getStreamState().getConsumerCount();
                 int co = cutoff.get();
                 if (siCount >= co) {
-                    if (co == maxConsumers) { // just to avoid repeat printing when full
+                    if (!reachedCutoff || co == maxConsumers) { // just to avoid repeat printing when full
                         cutoff.set(maxConsumers * 10 / 100); // 10 percent
                         print(produceJob, wctx, "* System is full. " + siCount + "/" + maxConsumers);
                     }
