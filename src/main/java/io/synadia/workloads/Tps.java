b60b98915c97a365d4d9a6680118f912f7c05749
scottf <scottfauerbach@gmail.com>
2025-10-21 17:41:37 -0400

    Testing 337

--- src/main/java/io/synadia/workloads/Tps.java
+++ src/main/java/io/synadia/workloads/Tps.java
@@ -188,23 +188,15 @@ public class Tps extends Workload {
 
             sendResults.add("\n" + TPS_SENDER);
             sendResults.add("Before Disconnect...");
-            sendResults.add(Debug.stringify("  Socket Written Payload Messages: %s",
+            sendResults.add(Debug.stringify("  Socket Written Messages: %s",
                 format3(sendStats.payloadCollector.writtenMessages)));
-            sendResults.add(Debug.stringify("  Socket Written Payload Bytes: %s",
+            sendResults.add(Debug.stringify("  Socket Written Bytes: %s",
                 format3(sendStats.payloadCollector.writtenBytes)));
-            sendResults.add(Debug.stringify("  Socket Written Protocol Messages: %s",
-                format3(sendStats.protocolCollector.writtenMessages)));
-            sendResults.add(Debug.stringify("  Socket Written Protocol Bytes: %s",
-                format3(sendStats.protocolCollector.writtenBytes)));
 
-            sendResults.add(Debug.stringify("  Buffered Payload Messages: %s",
+            sendResults.add(Debug.stringify("  Buffered Messages: %s",
                 format3(sendStats.payloadCollector.bufferedMessages)));
-            sendResults.add(Debug.stringify("  Buffered Payload Bytes: %s",
+            sendResults.add(Debug.stringify("  Buffered Bytes: %s",
                 format3(sendStats.payloadCollector.bufferedBytes)));
-            sendResults.add(Debug.stringify("  Buffered Protocol Messages: %s",
-                format3(sendStats.protocolCollector.bufferedMessages)));
-            sendResults.add(Debug.stringify("  Buffered Protocol Bytes: %s",
-                format3(sendStats.protocolCollector.bufferedBytes)));
 
             sendResults.add("Analysis ...");
             sendResults.add(Debug.stringify("  Last Write Messages: %s",
@@ -212,15 +204,10 @@ public class Tps extends Workload {
             sendResults.add(Debug.stringify("  Last Write Bytes: %s",
                 format3(sendStats.lastWriteBytes)));
 
-            sendResults.add(Debug.stringify("  Buffered Not Written Payload Messages: %s",
+            sendResults.add(Debug.stringify("  Buffered Not Written Messages: %s",
                 format3(sendStats.payloadCollector.notWrittenMessages)));
-            sendResults.add(Debug.stringify("  Buffered Not Written Payload Bytes: %s",
+            sendResults.add(Debug.stringify("  Buffered Not Written Bytes: %s",
                 format3(sendStats.payloadCollector.notWrittenBytes)));
-
-            sendResults.add(Debug.stringify("  Buffered Not Written Protocol Messages: %s",
-                format3(sendStats.protocolCollector.notWrittenMessages)));
-            sendResults.add(Debug.stringify("  Buffered Not Written Protocol Bytes: %s",
-                format3(sendStats.protocolCollector.notWrittenBytes)));
 
             List<String> skipList = sendWL.getGapList();
             if (skipList.isEmpty()) {
@@ -299,7 +286,7 @@ public class Tps extends Workload {
 
             terminate.await(10, TimeUnit.SECONDS);
 
-            receiveResults.addFirst(Debug.stringify("  Total Received Messages: %s", receivedMessages.get()));
+            receiveResults.addFirst(Debug.stringify("  Total Received Messages: %s", format3(receivedMessages.get())));
             receiveResults.addFirst("\n" + TPS_RECEIVER);
 
             currentDispatcher.unsubscribe(subject);
