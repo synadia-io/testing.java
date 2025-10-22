157699f70ac36e09ccc3216844f984bae23f7a53
scottf <scottfauerbach@gmail.com>
2025-10-21 21:15:06 -0400

    Testing 359

--- src/main/java/io/synadia/workloads/Tps.java
+++ src/main/java/io/synadia/workloads/Tps.java
@@ -299,7 +299,7 @@ public class Tps extends Workload {
             sleep(50);
             receiverReady.set(true);
 
-            if (!controlLatch.await(10, TimeUnit.SECONDS)) {
+            if (!controlLatch.await(15, TimeUnit.SECONDS)) {
                 Debug.info(TPS_RECEIVER, "!!!!! Terminate Message NOT Received");
             }
 
