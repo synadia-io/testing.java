c3a111fb29cbb81363b4c8409d82c66aca7cde0e
scottf <scottfauerbach@gmail.com>
2025-10-25 07:13:54 -0400

    Testing 405

--- src/main/java/io/nats/client/impl/TpsWriteListener.java
+++ src/main/java/io/nats/client/impl/TpsWriteListener.java
@@ -39,12 +39,12 @@ public class TpsWriteListener extends WriteListener {
     @Override
     public void buffered(NatsMessage msg, NatsConnectionWriter.Mode mode) {
         if (msg.getSubject() == null) {
-            Debug.info(label, msg);
+            Debug.info(label, mode, msg);
             protocolsBuffered.incrementAndGet();
             return;
         }
         if (msg.getSubject().equals(controlSubject)) {
-            Debug.info(label, msg);
+            Debug.info(label, mode, msg);
             controlsBuffered.incrementAndGet();
             return;
         }
